5decd5721784f755ff21b7c8ba8591946465e10a
razorboy73 <josh.kerbel@femme-o-nomics.com>
2014-01-15 09:47:04 -0500

    inital project

---
+++ models.py
@@ -0,0 +1,63 @@
+Required Tables
+
+
+
+User Registration
+ - primary key
+ - User Name
+ - Password
+ - First Name
+ - Last Name
+ - Company Name
+ - Email
+ - Phone Number
+ - Address line 1
+ - Address line 2
+ - Address line 3
+ - Postal/Zip code
+ - Province/State
+ - Country
+
+ - Confirmation email
+ - Prevent Duplication of company
+
+
+
+
+ - Admin rights
+User Log in
+ - Passwords/usernames
+
+Global Modifications
+ - Annual rent increase on anniversay of move in date
+ - Monthly Automatic Report Generation
+
+Building Registration
+ - Address
+ - Number of Units
+ - Rent per unit - handle this during tenant registration
+ - No duplications of buildings or unit
+ - Building Manager - recieves notifications via email
+
+Tenant Registration
+- First Name
+- Last Name
+- Rent per unit
+- Unit rented
+- Annual Increase over ride - Local Rent increase
+- Rental Effective Date
+- Move out date
+- Date rent due (default first of month)
+- handling terminations
+- Handling partial payments
+- What if tenant signs a lease before the current tenant moves out - 2 renters per apt?
+- NSF/Filing fees
+- AC Freezer Fees
+ - Possible duplication of names
+ - Monthly collection of Rent
+
+ Form Generation
+  - Leases
+  - Notices - preformatted forms
+  - Automated Late Rent Notice - automatically on day 2 of month and on demand - emailed automatically to manager in pdf
+  - Rent Increases - emailed automatically to manager
---
+++ user.py
@@ -0,0 +1,75 @@
+import sqlite3
+import datetime
+
+
+path = "/Users/workhorse/thinkful/"
+db = "apartment.db"
+
+def checkAndCreateDB():
+    #not checking for some reason
+    #fullPath = os.path.join(path, db)
+    #if os.path.exists(fullPath):
+    #   print "Database Exists"
+    #else:
+    connection = sqlite3.connect(db)
+    print "Creating database"
+    createUserRegTable()
+
+def createUserRegTable():
+    with sqlite3.connect(db) as connection:
+        c = connection.cursor()
+        c.execute("""CREATE TABLE People
+            (p_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            userName TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            confirmPassword TEXT NOT NULL,
+            firstName TEXT NOT NULL,
+            lastName TEXT NOT NULL,
+            companyName TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            phoneNumber TEXT NOT NULL,
+            addressLine1 TEXT NOT NULL,
+            addressLine2 TEXT,
+            addressLine3 TEXT,
+            zipCode TEXT NOT NULL,
+            province TEXT NOT NULL,
+            country TEXT NOT NULL,
+            regDate DATE NOT NULL)
+            """)
+        print "table made"
+
+
+def userSignIn():
+    pass
+
+def userSignUp():
+    userName = raw_input("Enter a user name: ")
+    password = raw_input("Enter a password: ")
+    confirmPassword = raw_input("Confirm Your Password: ")
+    firstName = raw_input("Enter your first name: ")
+    lastName = raw_input("Enter your last name: ")
+    companyName = raw_input("Enter your company name: ")
+    email = raw_input("Enter your email: ")
+    phoneNumber = raw_input("Enter your phone number: ")
+    addressLine1 = raw_input("Enter your address: ")
+    addressLine2 = raw_input("Enter second line of your address (Not Required): ")
+    addressLine3 = raw_input("Enter third line of your address (Not Required): ")
+    zipCode = raw_input("Enter your zip code: ")
+    province = raw_input("Enter your state or province: ")
+    country = raw_input("Enter your country: ")
+    regDate = datetime.date.today()
+    print regDate
+
+    userInfo = (userName, password, confirmPassword, firstName,lastName, companyName, email, phoneNumber,addressLine1,
+    addressLine2, addressLine3, zipCode, province, country, regDate)
+
+    with sqlite3.connect(db) as connection:
+        c = connection.cursor()
+        c.execute("INSERT INTO People VALUES(?, ?, ?, ?, ?,?, ?, ?, ?,?,?, ?, ?, ?, ?)",userInfo)
+
+checkAndCreateDB()
+
+userSignUp()
+
+
+
